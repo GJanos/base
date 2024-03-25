5e542906b4efaa2ac9ce77e673c161dfdadd202c
GJanos <janos.gorondi.official@gmail.com>
2024-03-25 18:21:29 +0100

    megint3

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,24 +5,24 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
-import hu.bme.mit.train.interfaces.TrainControllerImpl;
-import hu.bme.mit.train.interfaces.TrainSensorImpl;
-import hu.bme.mit.train.interfaces.TrainUserImpl;
+import hu.bme.mit.train.TrainControllerImpl;
+import hu.bme.mit.train.TrainSensorImpl;
+import hu.bme.mit.train.TrainUserImpl;
 
 public class TrainSensorTest {
 
     
-    TrainUserImpl mockTrainUser;
+    TrainUser mockTrainUser;
     
-    TrainControllerImpl mockController;
+    TrainController mockController;
 
     TrainSensorImpl trainSensor;
 
 
     @Before
     public void setUp() {
-        mockTrainUser = mock(TrainUserImpl.class);
-        mockController = mock(TrainControllerImpl.class);
+        mockTrainUser = mock(TrainUser.class);
+        mockController = mock(TrainController.class);
         trainSensor = new TrainSensor(mockController, mockTrainUser);
     }
 
