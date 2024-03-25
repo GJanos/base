f3bd47b08a61ece13a7b9fa882a77b7ac5054418
GJanos <janos.gorondi.official@gmail.com>
2024-03-25 16:43:26 +0100

    solution to referencespeed

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -3,13 +3,25 @@ package hu.bme.mit.train.user;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
 
+import java.util.*;
+
 public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private Timer t;
+	private TimerTask tt;
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
+		this.t = new Timer();
+		tt = new TimerTask() {  
+			@Override  
+			public void run() {  
+				overrideJoystickPosition(getJoystickPosition());
+			};
+		};
+		t.scheduleAtFixedRate(tt,1000,1000); 
 	}
 
 	@Override
@@ -25,7 +37,7 @@ public class TrainUserImpl implements TrainUser {
 	@Override
 	public void overrideJoystickPosition(int joystickPosition) {
 		this.joystickPosition = joystickPosition;
-		controller.setJoystickPosition(joystickPosition);
+		controller.setJoystickPosition(joystickPosition);	
 	}
 
 	@Override
